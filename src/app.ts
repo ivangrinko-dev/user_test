c7e456a605c1a1a60f31d1291355ac1cfcd2e0a0
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-12-26 21:59:16 +0300

    problems

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,15 @@ import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
 import user from "./controller/user.controller"
-
+import filiUpload from "express-fileupload"
 
 const app = express()
 
 app.use(bodyParser.json())
 
 app.use(cors())
+app.use(filiUpload({}))
+
 app.use(`/user`, user)
 
 
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import bildResponse from "../helper/bildresponse";
 import { createUser, getAllUser, getUserById, updateUserById, deleteUserById } from "../service/user.service";
+import { log } from "console";
 const route = express.Router()
-
-
+// import fileservice from "../fileservice/fileservice";
+//  import * as uuid from 'uuid';
+// import * as path from 'path';
 
 route.get(`/`, async (req, res): Promise<void> => {
     try {
@@ -27,8 +29,13 @@ route.get(`/:id`, async (req, res): Promise<void> => {
 
 route.post(`/`, async (req, res): Promise<void> => {
     try {
-        const { firstName, lastName, email, image, pdf} = req.body;
+        // const createdPost = await create({ image: fileName, pdf: fileName});
+        // console.log(req.files)
+        const {image} = req.files.image
+        const {pdf} = req.files.pdf
+        const { firstName, lastName, email} = req.body;
         const data = await createUser(firstName, lastName, email, image, pdf)
+        console.log(firstName, lastName, email, image, pdf)
         bildResponse(res, 200, data)
     } catch (error: any) {
         bildResponse(res, 404, error.message)
